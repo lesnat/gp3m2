aec19261f87e00ac91df4291ec1c502c24584b61
Léo <esnault.leo@orange.fr>
2018-08-28 18:14:06 +0200

    Update bench 00

--- benchmarks/00/analysis.py
+++ benchmarks/00/analysis.py
@@ -5,20 +5,21 @@ import sys
 if bench_path not in sys.path:sys.path.append(bench_path)
 import benchmark
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-# Import results and input file
-sim = benchmark.Data()
-ref = benchmark.Data()
+# Import results and ref input file
+sim = benchmark.Data(specie="e-")
+ref = benchmark.Data(specie="e-")
 sim.extract_G4("test_nt_electron_t*.csv",nthreads=2)
 ref.extract_txt("input.dat")
 
-print("Launching analysis script ...")
+# Artificially creates an error if needed
+# sim.w +=1.0
+# sim.py +=1.0
 
-# Instanciate an Error object with a maximum of 10 errors
-test = benchmark.Test(10)
+# Instanciate an Test object with a maximum display of 3 errors per test
+test = benchmark.Test(nerrors=3,rtol=1e-12)
+
+# Print initialization
+test.print_init(id="00")
 
 # Define number of events
 nevents = 1000
@@ -26,59 +27,34 @@ nevents = 1000
 # Test if number of output entries = number of primaries
 test.value('Number of events',len(sim.w),nevents)
 
-
-# Set relative tolerance
-test.rtol = 1e-2
 # Test total number of particles
-test.value('Total number of particles',np.trapz(sim.w),np.trapz(ref.w))
-# Test weight normalization
-test.vector('Weight normalization : sim in ref',sim.w*nevents,ref.w)
-test.vector('Weight normalization : ref in sim',ref.w,sim.w*nevents)
-"""
-# if
-# Test if the histogram of w is on same order of magnitude than W
-test.atol = 0.5
-# Number of primaries per entry. Each input row is used in average nopbe times. (here int)
-nopbe = nevents/len(W)
-# Make histos with weights
-hw,bw=np.histogram(w,bins=100)
-hW,bW=np.histogram(W,bins=100)
-# Calculate total weight per bin
-nw=hw*bw[1:]
-nW=hW*bW[1:]
-test.vector('nw/nW',(nw/nW)[nW!=0],[1])
-"""
+test.value('Total number of particles',sum(sim.w),sum(ref.w))
 
-# Set tolerance
-test.rtol = 1e-5
+# Test weight normalization
+wnorm=float(len(ref.w))/nevents
+test.vector('Weight normalization',sim.w/wnorm,ref.w)
 
 # Test if output momentum are present in the input
-test.vector('px : sim in ref',sim.px,ref.px)
-test.vector('py : sim in ref',sim.py,ref.py)
-test.vector('pz : sim in ref',sim.pz,ref.pz)
-
-# Test if all the input momentum are represented in the output
-test.vector('px : ref in sim',ref.px,sim.px)
-test.vector('py : ref in sim',ref.py,sim.py)
-test.vector('pz : ref in sim',ref.pz,sim.pz)
-
-
-# Set absolute tolerance to 0.01 (um)
-test.atol = 0.01
+test.vector('px',sim.px,ref.px)
+test.vector('py',sim.py,ref.py)
+test.vector('pz',sim.pz,ref.pz)
 
 # Define diag x coordinate (um)
 Dx = 50.
+
 # Test if x is in diag axis
-test.vector('x',sim.x,[Dx])
-# Test if the only x are at diag pos or 0
-test.vector('x',[Dx,0],sim.x)
+test.vector('Diagnostic position',sim.x,[Dx])
 
 # Calculate particle origin in y,z from momentum
 Dy = Dx*sim.py/sim.px
 Dz = Dx*sim.pz/sim.px
 # Test if it fit with input value
-test.vector('y-Dy',sim.y-Dy,ref.y)
-test.vector('z-Dz',sim.z-Dz,ref.z)
+test.vector('Origin of y',sim.y-Dy,ref.y)
+
+# Test if propagation time is correct
+Dt = sim.d()/sim.v()
+test.atol=1e-2 # precision of 1e-2 fs
+test.vector('Origin of t',sim.t-Dt,ref.t)
 
 # Print errors
-test.print_results()
+test.print_errors()
--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -1,76 +1,203 @@
 #coding:utf8
 
-import numpy as np
+import numpy as _np
 
 class Test(object):
-  def __init__(self,max_n):
+  """
+  Class to test values, collect and print errors.
+
+  Parameters
+  ----------
+  nerrors : int
+    maximum number of errors to print for each test
+  atol : float
+    absolute tolerance
+  rtol : float
+    relative tolerance
+  """
+  def __init__(self,nerrors,atol=0.0,rtol=0.0):
     self.text   = ""
-    self.max_n  = max_n
-    self.n      = 0
-    self.rtol   = 0.0
-    self.atol   = 0.0
+    self.nerrors= nerrors
+    self.error  = False
+    self.rtol   = rtol
+    self.atol   = atol
+    self.SEPARATOR="\n###############################################################\n"
 
-  def value(self,name,test_value,ref_value):
-    if not np.allclose(test_value,ref_value,rtol=self.rtol,atol=self.atol):
-      self.n      += 1
-      if self.max_is_reached():
-        pass
-      else:
-        self.text   += "    %(n)s\n\tvalue %(v)s does not fit with reference %(r)s,"%{'n':name,'v':test_value,'r':ref_value}
-        self.text   += " with atol=%(at)s and rtol=%(rt)s.\n"%{'at':self.atol,'rt':self.rtol}
+  def print_init(self,id):
+    """
+    Print a start message for the benchmarking.
 
-  def vector(self,name,test_vector,ref_vector):
-    for i,val in enumerate(test_vector):
-      if not self.is_in_ref(val,ref_vector):
-        self.n        += 1
-        if self.max_is_reached():
-          break
-        else:
-          self.text   += "    %(n)s\n\tvalue %(v)s not in the entries,"%{'n':name,'v':val}
-          self.text   += " with atol=%(at)s and rtol=%(rt)s"%{'at':self.atol,'rt':self.rtol}
-          self.text   += " (index = %(i)s).\n"%{'i':i}
+    Parameters
+    ----------
+    id : str
+      benchmark id
+    """
+    print(self.SEPARATOR)
+    print("Starting benchmark %s analysis ..."%id)
+    print(self.SEPARATOR)
 
-  def max_is_reached(self):
-    if self.n == self.max_n:
-      self.text       += "    ...\n"
-      self.text       += "    ...\n"
-      self.text       += "    ..."
-      return True
-    elif self.n > self.max_n:
-      return True
+  def print_result(self,label,n):
+    """
+    Print the result of a given test.
+
+    Parameters
+    ----------
+    label : str
+      name of the test
+    n : int
+      number of time the test failed
+    """
+    if n>0:
+      print(label+" ... ERROR ...")
     else:
-      return False
+      print(label+" ... OK !")
 
-  def is_in_ref(self,test_value,ref_vector):
-    is_in_ref=False
-    for _,rval in enumerate(ref_vector):
-      if np.allclose(rval,test_value,rtol=self.rtol,atol=self.atol):
-        is_in_ref=True
-        break
-    return is_in_ref
-
-  def print_results(self):
-    SEPARATOR="\n###############################################################\n"
-    print(SEPARATOR)
-    if self.n != 0:
+  def print_errors(self):
+    """
+    Print all the test errors.
+    """
+    print(self.SEPARATOR)
+    if self.error:
       print("Error(s) occured :\n")
       print(self.text)
     else:
       print("No error occured.")
-    print(SEPARATOR)
+    print(self.SEPARATOR)
+
+  def add_error(self,text):
+    """
+    Add an error to the buffer.
+
+    Parameters
+    ----------
+    text : str
+      text to add
+    """
+    self.error = True
+    self.text += text
+
+  def max_is_reached(self,n):
+    """
+    Test if the maximum number of errors is reached for a given test.
+
+    Parameters
+    ----------
+    n : int
+      number of time the test failed
+    """
+    if n == self.nerrors + 1:
+      self.text       += "\t...\n"
+      return True
+    elif n > self.nerrors:
+      return True
+    else:
+      return False
+
+  def is_in_ref(self,tval,rvect):
+    """
+    Test if the value tval is present in the vector rvect.
+
+    Parameters
+    ----------
+    tval : float or int
+      test value
+    rvect : list or np.array
+      reference vector
+    """
+    is_in_ref=False
+    for _,rval in enumerate(rvect):
+      if _np.allclose(rval,tval,rtol=self.rtol,atol=self.atol):
+        is_in_ref=True
+        break
+    return is_in_ref
+
+  def value(self,label,tval,rval):
+    """
+    Test if the tval and rval values are approximately the same.
+
+    Parameters
+    ----------
+    label : str
+      name of the test
+    tval : float or int
+      test value
+    rval : float or int
+      reference value
+    """
+    n = 0
+    if not _np.allclose(tval,rval,rtol=self.rtol,atol=self.atol):
+      n += 1
+      self.add_error(
+      """\t{label} :\n\t  value {tval} does not fit with reference {rval}, with atol={atol} and rtol={rtol}.\n"""
+      .format(label=label,tval=tval,rval=rval,atol=self.atol,rtol=self.rtol)
+      )
+
+    self.print_result(label,n)
+
+  def vector(self,label,tvect,rvect,both=True):
+    """
+    Test if the elements of tvect are in rvect.
+
+    Parameters
+    ----------
+    label : str
+      name of the test
+    tvect : list or np.array
+      test vector
+    rvect : list or np.array
+      reference vector
+    both : bool, optional
+      if True also test if elements of rvect are in tvect. Default is True
+    """
+    # Set current label, derived from given label
+    if both:
+      clabel = label  + " (test in ref)"
+    else:
+      clabel = label
+
+    # Test in ref
+    n = 0
+    for i,val in enumerate(tvect):
+      if not self.is_in_ref(val,rvect):
+        n += 1
+        if self.max_is_reached(n):
+          break
+        else:
+          self.add_error(
+          """\t{label} :\n\t  value {val} not in the entries, with atol={atol} and rtol={rtol} (index = {i}).\n"""
+          .format(label=clabel,val=val,atol=self.atol,rtol=self.rtol,i=i)
+          )
+    self.print_result(clabel,n)
+    # Ref in test
+    if both:
+      self.vector(label+" (ref in test)",rvect,tvect,both=False)
 
 
 class Data(object):
   """
   Class to extract and store simulation or input data from files.
+
+  Parameters
+  ----------
+  specie : str
+    label of the specie. Available are e-,e+,gamma
   """
+  def __init__(self,specie):
+    self.specie = specie
+    if specie in ("e-","e+","electron","positron"):
+      self.mass = 0.511
+    elif specie in ("gamma"):
+      self.mass = 0
+    else:
+      raise NameError("Unknown specie label")
+
   def extract_G4(self,file_name,nthreads=2):
     """
     Extract data from a csv Geant4 output file.
     """
     data = []
     fext = 'csv'   # File extension
-    fbase= file_name[:-5]   # File base name
+    fbase= file_name[:-5]   # File base label
     for thread in range(0,nthreads):
       fname=fbase+str(thread)+"."+fext
       with open(fname,'r') as f:
@@ -79,20 +206,20 @@ class Data(object):
             for e in line.split(','):
               data.append(float(e))
 
-    self.w   = np.array(data[0::8])
-    self.x   = np.array(data[1::8])
-    self.y   = np.array(data[2::8])
-    self.z   = np.array(data[3::8])
-    self.px  = np.array(data[4::8])
-    self.py  = np.array(data[5::8])
-    self.pz  = np.array(data[6::8])
-    self.t   = np.array(data[7::8])
+    self.w   = _np.array(data[0::8])
+    self.x   = _np.array(data[1::8])
+    self.y   = _np.array(data[2::8])
+    self.z   = _np.array(data[3::8])
+    self.px  = _np.array(data[4::8])
+    self.py  = _np.array(data[5::8])
+    self.pz  = _np.array(data[6::8])
+    self.t   = _np.array(data[7::8])
 
   def extract_txt(self,file_name):
     """
     Extract data from a text file
     """
-    w,x,y,z,px,py,pz,t=np.loadtxt(file_name).T
+    w,x,y,z,px,py,pz,t=_np.loadtxt(file_name).T
 
     self.w   = w
     self.x   = x
@@ -102,3 +229,39 @@ class Data(object):
     self.py  = py
     self.pz  = pz
     self.t   = t
+
+  def r(self):
+    return _np.sqrt(self.y**2 + self.z**2)
+
+  def d(self):
+    return _np.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+  def v(self):
+    return self.beta() * 2.99792458e8 * 1e6 / 1e15
+
+  def p(self):
+    return _np.sqrt(self.px**2 + self.py**2 + self.pz**2)
+
+  def ekin(self):
+    if self.mass==0:
+      return self.p()
+    else:
+      return (_np.sqrt((self.p()/self.mass)**2 + 1) - 1) * self.mass
+
+  def gamma(self):
+    if self.mass==0:
+      return self.ekin() # FIXME : vérifier
+    else:
+      return self.ekin()/self.mass + 1.
+
+  def beta(self):
+    if self.mass==0:
+      return _np.array([1]*len(self.w))
+    else:
+      beta =[]
+      for g in self.gamma():
+        if g>0:
+          beta.append(_np.sqrt(1 - (1/g)**2))
+        else:
+          beta.append(0)
+      return _np.array(beta)
