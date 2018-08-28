8ca6bd7e4ed09793faf00ac66f298564881579d6
Léo <esnault.leo@orange.fr>
2018-08-28 14:53:21 +0200

    Update bench 00

--- benchmarks/00/analysis.py
+++ benchmarks/00/analysis.py
@@ -8,57 +8,36 @@ import benchmark
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
+
 # Import results and input file
-bench = benchmark.Benchmark(
-        path        = ".",
-        ref_file    = "input.dat",
-        nthreads    = 2,
-        nevents     = 1000,
-        dpos        = 50.
-        )
-
-
-
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-"""
-# Import results and input file
-w,x,y,z,px,py,pz=benchmark.extract("test_nt_electron_t*.csv",nthreads=1)
-W,X,Y,Z,Px,Py,Pz,T=np.loadtxt("input.dat").T
+sim = benchmark.Data()
+ref = benchmark.Data()
+sim.extract_G4("test_nt_electron_t*.csv",nthreads=2)
+ref.extract_txt("input.dat")
 
 print("Launching analysis script ...")
 
 # Instanciate an Error object with a maximum of 10 errors
-error = benchmark.Error(10)
+test = benchmark.Test(10)
 
 # Define number of events
 nevents = 1000
 
 # Test if number of output entries = number of primaries
-error.test_value('len(w)',len(w),nevents)
+test.value('Number of events',len(sim.w),nevents)
 
 
 # Set relative tolerance
-error.rtol = 1e-2
-# Test weight normalization via integrate
-error.test_value('trapz(w)',np.trapz(w),np.trapz(W))
-
-###
+test.rtol = 1e-2
+# Test total number of particles
+test.value('Total number of particles',np.trapz(sim.w),np.trapz(ref.w))
+# Test weight normalization
+test.vector('Weight normalization : sim in ref',sim.w*nevents,ref.w)
+test.vector('Weight normalization : ref in sim',ref.w,sim.w*nevents)
+"""
 # if
 # Test if the histogram of w is on same order of magnitude than W
-error.atol = 0.5
+test.atol = 0.5
 # Number of primaries per entry. Each input row is used in average nopbe times. (here int)
 nopbe = nevents/len(W)
 # Make histos with weights
@@ -67,38 +46,39 @@ hW,bW=np.histogram(W,bins=100)
 # Calculate total weight per bin
 nw=hw*bw[1:]
 nW=hW*bW[1:]
-error.test_vector('nw/nW',(nw/nW)[nW!=0],[1])
-###
+test.vector('nw/nW',(nw/nW)[nW!=0],[1])
+"""
 
 # Set tolerance
-error.rtol = 1e-5
+test.rtol = 1e-5
 
 # Test if output momentum are present in the input
-error.test_vector('px',px,Px)
-error.test_vector('py',py,Py)
-error.test_vector('pz',pz,Pz)
+test.vector('px : sim in ref',sim.px,ref.px)
+test.vector('py : sim in ref',sim.py,ref.py)
+test.vector('pz : sim in ref',sim.pz,ref.pz)
 
 # Test if all the input momentum are represented in the output
-error.test_vector('Px',Px,px)
-error.test_vector('Py',Py,py)
-error.test_vector('Pz',Pz,pz)
+test.vector('px : ref in sim',ref.px,sim.px)
+test.vector('py : ref in sim',ref.py,sim.py)
+test.vector('pz : ref in sim',ref.pz,sim.pz)
 
 
 # Set absolute tolerance to 0.01 (um)
-error.atol = 0.01
+test.atol = 0.01
 
 # Define diag x coordinate (um)
 Dx = 50.
 # Test if x is in diag axis
-error.test_vector('x',x,[Dx])
+test.vector('x',sim.x,[Dx])
+# Test if the only x are at diag pos or 0
+test.vector('x',[Dx,0],sim.x)
 
 # Calculate particle origin in y,z from momentum
-Dy = Dx*py/px
-Dz = Dx*pz/px
+Dy = Dx*sim.py/sim.px
+Dz = Dx*sim.pz/sim.px
 # Test if it fit with input value
-error.test_vector('y-Dy',y-Dy,Y)
-error.test_vector('z-Dz',z-Dz,Z)
+test.vector('y-Dy',sim.y-Dy,ref.y)
+test.vector('z-Dz',sim.z-Dz,ref.z)
 
 # Print errors
-error.print_results()
-"""
+test.print_results()
